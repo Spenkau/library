25ce92f43a9bdc60846c82781c5ad61cfc59ef03
Spenkau <96494361+Spenkau@users.noreply.github.com>
2022-04-01 20:04:59 +0300

    source file changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>Book store</h3>
+            <p>Reading is the best teaching</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>X BYN</span>
+          </li>
+          <li>
+            <svg />
+            <span></span>
+          </li>
+        </ul>
       </header>
     </div>
   );
